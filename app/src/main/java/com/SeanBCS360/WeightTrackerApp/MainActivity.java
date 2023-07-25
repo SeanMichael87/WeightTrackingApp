07c5ddd8113a30cdfcaa5a547f5aed137a37621c
Sean B <Celtsfan9212013@gmail.com>
2023-07-24 21:44:13 -0400

    sms_permissions update with basic functionality

--- app/src/main/java/com/SeanBCS360/WeightTrackerApp/MainActivity.java
+++ app/src/main/java/com/SeanBCS360/WeightTrackerApp/MainActivity.java
@@ -1,14 +1,38 @@
 package com.SeanBCS360.WeightTrackerApp;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        loginButton = findViewById(R.id.login);
+
+        loginButton.setOnClickListener(view -> showCustomDialog());
+    }
+
+    void showCustomDialog() {
+        Button accept;
+        Button deny;
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.sms_permissions);
+
+        accept = dialog.findViewById(R.id.button_accept);
+        deny = dialog.findViewById(R.id.button_deny);
+
+        accept.setOnClickListener(view -> dialog.dismiss());
+        deny.setOnClickListener(view -> dialog.dismiss());
+
+        dialog.show();
+
     }
 }
