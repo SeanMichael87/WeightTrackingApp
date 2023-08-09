5ab50910057c73569ee011fd455c8f0b6dccd860
Sean <79989128+SeanMichael87@users.noreply.github.com>
2023-08-09 08:24:57 -0400

    Create ForgotPassDialog.java

---
+++ app/src/main/java/com/SeanBCS360/WeightTrackerApp/ForgotPassDialog.java
@@ -0,0 +1,68 @@
+package com.SeanBCS360.WeightTrackerApp;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link } factory method to
+ * create an instance of this fragment.
+ */
+public class SignUpDialog extends DialogFragment {
+
+    public DBHandler db;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View v = inflater.inflate(R.layout.dialog_get_pass, null);
+
+        EditText phoneNumber = v.findViewById(R.id.phone_number);
+
+        db = new DBHandler(getActivity());
+
+        calDate.setOnClickListener(view -> {
+            String phoneNum = phoneNumber.getText().toString();
+
+        });
+
+        builder.setView(v)
+                .setPositiveButton("Sign Up", (dialog, id) -> {
+
+                  
+                })
+                .setNegativeButton("Quit", (dialog, id) -> {
+
+                    CharSequence text = "Come Back Soon!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast.makeText(getActivity(), text, duration).show();
+                });
+
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+
+}
